c42b78535a7aa7e01750f389fb3b277001a59a82
Huysmans Werner <huysmanw@AKDGPORT8474.local>
2018-08-29 16:22:50 +0200

    more lowercase

--- components/FicheContent.vue
+++ components/FicheContent.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="fiche-content">
       <section v-if="fiche.Beschrijving" id="beschrijving" class="article-section">
-          <h2 class="article-section-title">Beschrijving</h2>
+          <h2 class="article-section-title">beschrijving</h2>
           <span :class="['icon is-medium', color]">
                   <i class="far fa-hand-point-right" aria-hidden="true"></i>
           </span>
           <div class="editor-content" v-html="highlight(fiche.Beschrijving)"></div>
       </section>
       <section v-if="fiche.Werking" id="werking" class="article-section">
-          <h2 class="article-section-title">Werking</h2>
+          <h2 class="article-section-title">werking</h2>
           <span :class="['icon is-medium', color]">
                   <i class="fas fa-cogs" aria-hidden="true"></i>
           </span>
           <div class="editor-content" v-html="highlight(fiche.Werking)"></div>
       </section>
       <section v-if="fiche.Tips" id="tips" class="article-section">
-          <h2 class="article-section-title">Tips</h2>
+          <h2 class="article-section-title">tips</h2>
           <span :class="['icon is-medium', color]">
                   <i class="far fa-lightbulb" aria-hidden="true"></i>
           </span>
           <div class="editor-content" v-html="highlight(fiche.Tips)"></div>
       </section>
       <section v-if="fiche.Voorbeelden" id="voorbeelden" class="article-section">
-          <h2 class="article-section-title">Voorbeelden</h2>
+          <h2 class="article-section-title">voorbeelden</h2>
           <span :class="['icon is-medium', color]">
                   <i class="far fa-image" aria-hidden="true"></i>
           </span>
           <div class="editor-content" v-html="highlight(fiche.Voorbeelden)"></div>
       </section>
       <section v-if="fiche.Reflectie" id="reflectie" class="article-section">
-          <h2 class="article-section-title">Reflectie</h2>
+          <h2 class="article-section-title">reflectie</h2>
           <span :class="['icon is-medium', color]">
                   <i class="fab fa-twitch" aria-hidden="true"></i>
           </span>
