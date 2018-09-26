d42b65cc53cb00e3279635ed1ef0eb8633c96e68
Huysmans Werner <huysmanw@AKDGPORT7921.admin.kdg.be>
2018-09-26 11:14:42 +0200

    typo fixed

--- pages/samenwerking.vue
+++ pages/samenwerking.vue
@@ -5,7 +5,7 @@
         <h1 class="article-title">Samenwerking</h1>
       </header>
       <section class="article-content columns">
-        <div class="editor-content column">In dit project werken we samen met de Universiteit Antwerpen (UA) en de Vrije Universiteit Brussel (VUB). Taalleerkrachten uit vier secundaire scholen en studenten uit de lerarenopleiding (KdG en UA) engageerden zich om de toolbox samen met de onderzoekers van KdG op te bouwen en verder vorm te geven</div>
+        <div class="editor-content column">In dit project werken we samen met de Universiteit Antwerpen (UA) en de Vrije Universiteit Brussel (VUB). Taalleerkrachten uit vier secundaire scholen en studenten uit de lerarenopleiding (KdG en UA) engageerden zich om de toolbox samen met de onderzoekers van KdG op te bouwen en verder vorm te geven.</div>
       </section>
     </article>
   </section>
