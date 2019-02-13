072477bfada5e2168e66ad8612bcde9ee06624bb
Huysmans Werner <huysmanw@AKDGPORT8474.local>
2019-02-13 11:00:21 +0100

    samenwerking changed

--- pages/samenwerking.vue
+++ pages/samenwerking.vue
@@ -5,11 +5,11 @@
         <h1 class="article-title">Samenwerking</h1>
       </header>
       <section class="article-content columns">
-        <div class="editor-content column">In dit project werken we samen met de Universiteit Antwerpen (UA) en de Vrije Universiteit Brussel (VUB). Taalleerkrachten uit vier secundaire scholen en studenten uit de lerarenopleiding (KdG en UA) engageerden zich om de toolbox samen met de onderzoekers van KdG op te bouwen en verder vorm te geven.</div>
+        <div class="editor-content column">Voor dit project werkte Karel de Grote Hogeschool (KdG) samen met de Universiteit Antwerpen (UA) en de Vrije Universiteit Brussel (VUB). Taalleerkrachten uit vier secundaire scholen en studenten uit de lerarenopleiding (KdG en UA) engageerden zich om de toolbox samen met de onderzoekers van KdG op te bouwen en verder vorm te geven.</div>
       </section>
       <aside class="article-meta editor-content box">
         Partnerscholen: Gemeentelijk Technisch Instituut Sint-Jozef Kalmthout, Sint-Jozefinstituut Kontich, Sint-Michielscollege Schoten, Sint-Norbertusinstituut Duffel.
-        <br />KdG-medewerkers: Debbie De Neve, An Leroy, Werner Huysmans, Erika Rosiers, Wouter Smets.
+        <br />KdG-medewerkers: Debbie De Neve, An Leroy, Werner Huysmans, Dieter Reynaert, Erika Rosiers, Wouter Smets.
         <br />Contact: debbie.deneve@kdg.be
       </aside>
     </article>
