37b611c94af6ebddef9ef1cd0eafa28a8122daa4
Huysmans Werner <huysmanw@AKDGPORT8474.local>
2021-07-06 15:01:53 +0200

    samenwerking update

--- pages/samenwerking.vue
+++ pages/samenwerking.vue
@@ -12,6 +12,14 @@
         <br />KdG-medewerkers: Debbie De Neve, An Leroy, Werner Huysmans, Dieter Reynaert, Erika Rosiers, Wouter Smets.
         <br />Contact: debbie.deneve@kdg.be
       </aside>
+      <section class="article-content columns">
+        <div class="editor-content column"><h3>Intellectuele eigendomsrechten</h3>
+De gebruiker heeft het recht om de informatie op de website (waaronder o.m. al het redactioneel materiaal, de informatieve teksten, foto's, illustraties, tekeningen en ander grafisch materiaal, namen, logo's, handelsmerken en andere merken) te consulteren en enkel en alleen voor persoonlijk gebruik te downloaden en te reproduceren, mits hij de bron vermeldt, overeenkomstig de bepalingen van de wet van 30 juni 1994 betreffende het auteursrecht en de naburige rechten.
+ 
+Ieder ander gebruik van de informatie en materialen of iedere andere vorm van reproductie ervan, hetzij geheel of gedeeltelijk, is slechts toegelaten na een voorafgaande schriftelijke toestemming van KdG.
+In geen geval mag de gebruiker de inhoud van de website en de daarop aangeboden informatie en materialen gebruiken voor commerciële doeleinden, noch deze verdelen of meedelen aan derden, al dan niet met winstoogmerk, op een wijze die afbreuk kan doen aan de normale exploitatie ervan door KdG, zonder voorafgaande uitdrukkelijke schriftelijke toestemming van KdG.
+KdG behoudt alle huidige en toekomstige intellectuele eigendomsrechten op de website zelf en op de ter beschikking gestelde informatie en materialen.”</div>
+      </section>
     </article>
   </section>
 </template>
